fb9b48225056cc2444f62e31efba464df90f0b2f
Avadhoot Bhogil <58785159+Avadhoot5@users.noreply.github.com>
2024-08-01 16:38:04 +0530

    typescript learning started

---
+++ Week 7 practice/Week7.2.js
@@ -0,0 +1,6 @@
+function sum(a, b) {
+    return a + b;
+}
+var ans = sum(2, 5);
+console.log("test");
+console.log(ans);
---
+++ Week 7 practice/Week7.2.ts
@@ -0,0 +1,8 @@
+function sum(a:number, b: number): number {
+    return a + b;
+}
+
+let ans = sum(2,5);
+console.log("test");
+console.log(ans);
+
