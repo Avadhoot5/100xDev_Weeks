f63292f8b4199f888f3e884a5f5c06e8bbebaf76
Avadhoot Bhogil <58785159+Avadhoot5@users.noreply.github.com>
2024-10-02 20:16:26 +0530

    added GET, POST routes - query headers body

--- Practice/Week 2/week2.1.js
+++ Practice/Week 2/week2.1.js
@@ -2,11 +2,38 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.json({message: 'Hello World!'});
-    
+    res.json({message: 'Hello World!'});  
 })
+
+app.get('/calculateSum', (req, res) => {
+    let value = parseInt(req.query.counter);
+    let sum = ((value) * (value + 1))/2;
+    res.json({message: 'Sum is ' + sum});
+})
+
+app.get('/calculateSum/:value', (req, res) => {
+    let value = parseInt(req.params.value);
+    let sum = ((value) * (value + 1))/2;
+    res.json({message: 'Sum is ' + sum});
+})
+
+// Post request - Headers
+
+app.post('/valueHeader', (req, res) => {
+    const value = req.headers.value;
+    res.json({message: 'You posted this value through headers:' + value})
+})
+
+// Post request - body
+
+app.post('/valueBody', (req, res) => {
+    const {value} = req.body;
+    res.json({message: 'Value read through body is: ' + value});
+})
+
 
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`);
