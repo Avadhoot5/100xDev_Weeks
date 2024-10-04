049a14bfe44810b9f80d7a8d54b2915788142528
Avadhoot Bhogil <58785159+Avadhoot5@users.noreply.github.com>
2024-10-04 10:25:53 +0530

    added POST, delete routes for todo

--- Practice/Week 3/week3.1.js
+++ Practice/Week 3/week3.1.js
@@ -32,6 +32,65 @@ app.get('/todos/:id', (req, res) => {
   });
 })
 
+// post todo
+app.post('/todos', (req, res) => {
+  const {title, description} = req.body;
+  const id = new Date().getTime();
+
+  fs.readFile('data.json', 'UTF-8', (err, data) => {
+    if (err) console.log('file not found');
+    const todos = JSON.parse(data);
+    const todo = {
+      title, description, id
+    }
+    todos.push(todo);
+    res.status(201).json({id: id});
+    fs.writeFile('data.json', JSON.stringify(todos), 'UTF-8', (err, data) => {
+      if (data) return;
+    })
+  })
+})
+
+// post todo
+app.put('/todos/:id', (req, res) => {
+  const {title, description} = req.body;
+  const id = parseInt(req.params.id);
+
+  fs.readFile('data.json', 'UTF-8', (err, data) => {
+    if (err) console.log('file not found');
+    const todos = JSON.parse(data);
+    const todoIndex = todos.findIndex((a) => a.id === id);
+    if (todoIndex < 0) return res.status(404).json({message: 'Todo ID not found'});
+    const updatedTodo = {
+      title, description, id
+    }
+    todos[todoIndex] = updatedTodo; 
+    res.status(200).json({message: 'todo item was updated'})
+    fs.writeFile('data.json', JSON.stringify(todos), 'UTF-8', (err, data) => {
+      if (data) return;
+    })
+  })
+})
+
+app.delete('/todos/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+
+  fs.readFile('data.json', 'UTF-8', (err, data) => {
+    const todos = JSON.parse(data);
+    const todoIndex = todos.find((a) => a.id === id);
+
+    if (!todoIndex) return res.status(400).json({message: 'Todo ID not found'});
+
+    const filterTodo = todos.filter((a) => a.id !== id);
+    if (filterTodo) res.status(200).json({message: 'Todo item deleted'});
+
+    fs.writeFile('data.json', JSON.stringify(filterTodo), 'UTF-8', (err, data) => {
+      if (data) return;
+    })
+
+  })
+})
+
 app.listen(PORT,() => {
   console.log(`App listening on port ${PORT}`)
 });
